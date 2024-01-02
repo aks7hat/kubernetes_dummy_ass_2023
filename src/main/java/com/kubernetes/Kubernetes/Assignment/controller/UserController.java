0e0416eab89388e38b96d245ba9e18fe2510e7b4
aks7hat <54040109+aks7hat@users.noreply.github.com>
2024-01-02 17:32:25 +0530

    Update UserController.java

--- src/main/java/com/kubernetes/Kubernetes/Assignment/controller/UserController.java
+++ src/main/java/com/kubernetes/Kubernetes/Assignment/controller/UserController.java
@@ -1,16 +1,33 @@
 package com.kubernetes.Kubernetes.Assignment.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.ui.Model;
 
 
 @Controller
 @RequestMapping("/home")
 public class UserController {
+	
+	@Value("${application.password}")
+	private String applicationPassword;
+	
+	@Value("${application.name}")
+	private String applicationName;
+	
+	@Value("${application.connectionstring}")
+	private String connectionString;
+	
+	@Value("${application.environment}")
+	private String applicationEnvironment;
 	
 	@GetMapping("/form")
 	public String inputForm() {
@@ -21,6 +38,17 @@ public class UserController {
     public String submitForm(@RequestParam String name, Model model) {
         model.addAttribute("name", name);
         return "result";
+    }
+    
+    @GetMapping("/getEnv")
+    @ResponseBody
+    public Map<String, String> getEnvVariables() {
+        Map<String, String> configMap = new HashMap<>();
+        configMap.put("Application_Password", applicationPassword);
+        configMap.put("Application_Environment", applicationEnvironment);
+        configMap.put("Application_Name", applicationName);
+        configMap.put("Application_ConnectionString", connectionString);
+        return configMap;
     }
 
 }
